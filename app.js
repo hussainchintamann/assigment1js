297f40a749c9da0f50a412c6ad98c1b22c1e4da3
hussainchintamann <hussaindr369@gmail.com>
2024-01-29 10:59:32 +0500

    Fisrt Assigment js commplete

---
+++ app.js
@@ -0,0 +1,255 @@
+// ans1-start
+
+alert("Welcome to Our Website")
+
+// ans1-end
+
+
+// ans2-start
+
+document.write("Error! Please Enter a valid password" + "<br>" ) 
+
+// ans2-end
+
+// ans3-start
+
+document.write("Use Line Breake" ) 
+
+// ans3-end
+
+
+// ans4-start
+
+var a=(" Enter your Correct Name ");
+var b=(" Enter your Correct Password ");
+var c=(" Enter your Correct Email");
+alert(a);
+alert(b);
+alert(c);
+
+// ans4-end
+
+// ans5-start
+
+// ans5-end
+
+
+
+// ans6-start
+
+// ans6-end
+
+// ans7-start
+
+
+var userName = ("Muhammad Hussain");
+alert(userName);
+
+// ans7-end
+
+
+
+// ans8-start
+
+var myName =("Muhammad Hussain");
+alert( "My Name Is="+  myName );
+
+// ans8-end
+
+
+//  ans9-start
+
+var titledMessage= ("Hello World");
+alert( titledMessage);
+
+// ans9-end
+
+//  ans10-start
+
+var myName =("Muhammad Hussain");
+var fatherName =("Muhammad Hanif" ); 
+var birth = ("21/Nov/2001");
+var gender =("Male");
+var bio = " Name="+ myName+ "                                                                  "+ "Father Name="+fatherName+ "                                                            " + " Birth Day="+birth+ " " + " Gender="+gender;
+alert(bio);
+ans10-end
+
+
+//  ans11-start
+
+var a = ("Email Address");
+var b =("Hussaindr369@gamil.com");
+var c = a+" :" + " " +b;
+alert(c);
+ans11-end
+
+
+//  ans12-start
+
+        document.write("Hello friend! How are You");
+
+// ans12-end
+
+
+//  ans13-start
+var frame = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+alert(frame);
+document.write(frame);
+
+// ans13-end
+
+//  ans14-start
+
+var x =("My Age Is");
+var y = ("22");
+var z = x+ " :"+ " " +y;
+alert(z);
+
+    
+
+// ans14-end
+
+//  ans15-start
+
+var message=("You have visited this site ");
+var visit =("500");
+var ans =message +visit + " "+"times";
+document.write(ans);    
+
+// ans15-end
+
+
+//   ans16-start
+
+var birthYear = ("My Birth Year Is 2001");
+document.write(birthYear);
+
+
+// ans16-end
+
+//   ans17-start
+
+var name = ("JohnDoe");
+var product = ("Shirts");
+var quentity =("100");
+var ans =  " " + "Visitor Name" + " =" + " " +name + " " + "Product Name" + " =" + " " +product + " " + "Quentity" + " =" + " " +quentity 
+document.write(ans);
+
+// ans17-end
+
+// ans18-start
+
+var a = ("Hello world");
+var b = ("How Are You");
+var c = ("what are you doing");
+alert(a);
+alert(b);
+alert(c);
+
+// ans18-end
+
+
+
+
+// ans19-start
+document.write(" camel case (firstName)"+"<br>" + "snak case (first_name)" + "<br>" + " Dollar sign  ($name) " + "<br>"+"Alphabet+Number (A232)"+ "<br>" +"Underscore" + "<br>")
+// **Legal variable names:**
+// camel case (firstName)
+// snak case (first_name)
+// Dollar sign  ($name)
+// Alphabet+Number (A232)
+// Underscore
+
+// **Illegal variable names:**
+
+document.write("`123abc` (cannot start with a number)"+"<br>" + "`first-name` (cannot contain hyphens)" + "<br>" + "`my variable` (cannot contain spaces) " + "<br>"+"`my.variable` (cannot contain periods)"+ "<br>")
+// - `123abc` (cannot start with a number)
+// - `first-name` (cannot contain hyphens)
+// - `my variable` (cannot contain spaces)
+// - `my.variable` (cannot contain periods)
+
+// ans19-end
+
+
+
+
+// ans20-start
+
+document.write("<br> 1= Variable names can only contain Alphabetics, Digits, Under score and dollar sign");
+document.write ("<br>2= Variables must begin with a Alphabetic, Digits and Under Score  ");
+document.write("")
+document.write("<br> 3= Variable names should not be JS space ");
+
+// ans20-end
+
+// ans21-start
+var a = 20;
+var b = 50;
+var c =a+b;
+document.write(c);
+// ans21-end
+
+
+// ans22-start
+
+var a=30
+document.write("<br>" + "30-10"+"=" +a-10);
+document.write("<br>" + "30x10"+"=" +a * 10);
+document.write("<br>" + "30/10"+"=" +a / 10);
+document.write("<br>" + "30%10"+"=" +a % 10);
+
+// ans22-end
+
+
+// ans23-start
+
+
+var myNum;
+document.write("Value after variable declaration is: " + myNum + "<br>")
+
+var myNum =5;
+document.write("Initial value:"+ myNum +"<br>") ;
+
+myNum++
+
+document.write("Value after increment is: " + myNum + "<br>")
+
+myNum += 7;
+
+document.write("Value After Addition is " + myNum +"<br>")
+
+myNum -=1;
+document.write("Value after decrement is: " + myNum + "<br>")
+
+myNum %=3;
+
+document.write(" The remainder is :" + myNum +"<br>" )
+// ans23-end
+
+
+// ans24-start
+
+var num1=600;
+var num2=5;
+var ans =num1 * num2;
+alert(ans);     
+// ans24-end
+
+
+// ans25-start
+
+var a = 1;
+while(a<82)
+{
+   if(a%8==0)
+{
+   document.write(a+ "<br>");
+}
+   a++;
+}
+
+// ans25-end
+
+
+
+
